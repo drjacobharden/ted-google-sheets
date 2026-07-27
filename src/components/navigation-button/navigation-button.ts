f454ab7ad36b5cfe9228f824ee3b09fff24822df
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-26 23:26:00 -0400

    Created a navigation button component

---
+++ src/components/navigation-button/navigation-button.ts
@@ -0,0 +1,39 @@
+import { Icons } from "../../icons";
+import templateString from "./template.html" with { type: "text" };
+
+const navigationButtonTemplate = document.createElement("template");
+navigationButtonTemplate.innerHTML = templateString;
+
+class NavigationButton extends HTMLElement {
+  #label: HTMLElement | null = null;
+  #icon: SVGElement | null = null;
+
+  static get observedAttributes(): string[] {
+    return ["icon", "label", "class"];
+  }
+
+  connectedCallback(): void {
+    const clone = navigationButtonTemplate.content.cloneNode(
+      true,
+    ) as DocumentFragment;
+    const button = clone.querySelector("button") as HTMLElement;
+
+    const label = this.getAttribute("label");
+    const icon = this.getAttribute("icon");
+
+    this.#label = button.querySelector("span");
+    this.#icon = button.querySelector(".icon-wrapper");
+
+    if (label && this.#label) {
+      this.#label.textContent = label;
+    }
+
+    if (icon && Icons[icon] && this.#icon) {
+      this.#icon.innerHTML = Icons[icon];
+    }
+
+    this.appendChild(clone);
+  }
+}
+
+customElements.define("navigation-button", NavigationButton);
