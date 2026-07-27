d131f95325816b49a1f2076a1cda0a0f7c534293
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-27 13:29:59 -0400

    Icon adjustments

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,25 +1,27 @@
 import dashboardIcon from "./dashboard.html" with { type: "text" };
 import transcationsIcon from "./transactions.html" with { type: "text" };
-import categoryIcon from "./category.html" with { type: "text" };
-import vendorIcon from "./vendors.html" with { type: "text" };
+import labelIcon from "./label.html" with { type: "text" };
 import peopleIcon from "./people.html" with { type: "text" };
-import investmentIcon from "./investments.html" with { type: "text" };
-import accountsIcon from "./accounts.html" with { type: "text" };
+import chartIcon from "./chart.html" with { type: "text" };
+import boxIcon from "./box.html" with { type: "text" };
 import plusIcon from "./plus.html" with { type: "text" };
 import importIcon from "./import.html" with { type: "text" };
 import syncIcon from "./sync.html" with { type: "text" };
 import settingsIcon from "./settings.html" with { type: "text" };
+import cartIcon from "./cart.html" with { type: "text" };
+import sidebarIcon from "./sidebar.html" with { type: "text" };
 
 export const Icons: Record<string, string> = {
   dashboard: dashboardIcon,
   transactions: transcationsIcon,
-  category: categoryIcon,
-  vendors: vendorIcon,
+  label: labelIcon,
   people: peopleIcon,
-  investments: investmentIcon,
-  accounts: accountsIcon,
+  chart: chartIcon,
+  box: boxIcon,
   plus: plusIcon,
   import: importIcon,
   sync: syncIcon,
   settings: settingsIcon,
+  cart: cartIcon,
+  sidebar: sidebarIcon,
 };
