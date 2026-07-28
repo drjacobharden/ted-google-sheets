3a053d0e18c38a23e77b424b76c8993e0ac06a2f
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-28 18:31:07 -0400

    Renamed router to main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import "./components/page-title/page-title.ts";
 import "./components/navigation-button/navigation-button.ts";
 import "./templates/navigation-bar/navigation-bar.ts";
-//
+import "./templates/overlay-manager/overlay-manager.ts";
+import "./components/tooltip/tooltip.ts";
+
+////
