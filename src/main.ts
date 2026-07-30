1ec601c086afcafa0f3ad740dc9341d068813c48
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-30 06:41:24 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import "./components/navigation-button/navigation-button.ts";
 import "./templates/navigation-bar/navigation-bar.ts";
 import "./templates/overlay-manager/overlay-manager.ts";
 import "./components/tooltip/tooltip.ts";
+import "./components/breadcrumbs/breadcrumbs.ts";
+import "./components/refresh-indicator/refresh-indicator.ts";
+import "./components/splash-indicator/splash-indicator.ts";
 
+import "./screens/transactions/transactions.ts";
 ////
+
+(function () {})();
