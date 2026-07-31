7b41e04929ac975927c33e800de570ed0a4f4997
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-31 12:00:41 -0400

    Update router.ts

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,36 +5,26 @@ const ROUTER_CONFIG: RouterConfig = {
   dashboard: {
     section: "Budget",
     template: "route-dashboard",
-    script: "js/routes/dashboard.js",
-    module: () => window.DashboardRoute,
   },
 
   categories: {
     section: "Budget",
     template: "route-categories",
-    script: "js/routes/categories.js",
-    module: () => window.CategoryRoute,
   },
 
   vendors: {
     section: "Budget",
     template: "route-vendors",
-    script: "js/routes/vendors.js",
-    module: () => window.VendorRoute,
   },
 
   people: {
     section: "Budget",
     template: "route-people",
-    script: "js/routes/people.js",
-    module: () => window.PeopleRoute,
   },
 
   import: {
     section: "Utilities",
     template: "route-import",
-    script: "js/routes/import.js",
-    module: () => window.ImportRoute,
   },
 
   transactions: {
@@ -47,49 +37,35 @@ const ROUTER_CONFIG: RouterConfig = {
   "entity-detail": {
     section: "Budget",
     template: "route-entity-detail",
-    script: "js/routes/entity-detail.js",
-    module: () => window.EntityRoute,
   },
 
   "entity-archive": {
     section: "Budget",
     template: "route-entity-archive",
-    script: "js/routes/entity-archive.js",
-    module: () => window.EntityArchiveRoute,
   },
 
   settings: {
     section: "Utilities",
     template: "route-settings",
-    script: "js/routes/settings.js",
-    module: () => window.SettingsRoute,
   },
 
   "investment-overview": {
     section: "Investments",
     template: "route-investment-overview",
-    script: "js/routes/investment-overview.js",
-    module: () => window.InvestmentOverviewRoute,
   },
 
   "investment-accounts": {
     section: "Investments",
     template: "route-investment-accounts",
-    script: "js/routes/investment-accounts.js",
-    module: () => window.InvestmentAccountsRoute,
   },
 
   "investment-account-detail": {
     section: "Investments",
     template: "route-investment-account-detail",
-    script: "js/routes/investment-account-detail.js",
-    module: () => window.InvestmentAccountDetailRoute,
   },
 
   sync: {
     section: "Utilities",
     template: "route-sync",
-    script: "js/routes/sync.js",
-    module: () => window.SyncRoute,
   },
 };
