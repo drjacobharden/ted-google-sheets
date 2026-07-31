3d5339f0f611ebe9c4076a194725c3488cc0726f
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-31 12:00:13 -0400

    Update build.ts

--- build.ts
+++ build.ts
@@ -10,3 +10,7 @@ if (!result.success) {
   console.error(result.logs);
   process.exit(1);
 }
+
+const bundlePath = "./dist/bundle.js";
+const bundle = await Bun.file(bundlePath).text();
+await Bun.write(bundlePath, `(() => {\n${bundle}\n})();\n`);
