2650ed74bf9f7118e715b43dc5216c8ccffa4202
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-31 12:00:47 -0400

    Update declarations.d.ts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -5,6 +5,13 @@ declare global {
   }
 
   module "*.css";
+
+  interface Window {
+    BudgetAPI: import("./api/budget-api.ts").BudgetAPIContract;
+    InvestmentAPI: import("./api/investment-api.ts").InvestmentAPIContract;
+    ImportAPI: import("./api/import-api.ts").ImportAPIContract;
+    ImportUtils: import("./utilities/import-runtime.ts").ImportUtilities;
+  }
 }
 
 export {};
