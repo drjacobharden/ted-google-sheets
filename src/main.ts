1ec86be2fd9c696c4c016fbeb4ab9eaf17f446f1
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-07-31 12:00:37 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
+import "./api/api.ts";
+
 import "./components/page-title/page-title.ts";
 import "./components/button/button.ts";
 import "./components/navigation-button/navigation-button.ts";
-import "./templates/navigation-bar/navigation-bar.ts";
-import "./templates/overlay-manager/overlay-manager.ts";
 import "./components/tooltip/tooltip.ts";
 import "./components/breadcrumbs/breadcrumbs.ts";
 import "./components/refresh-indicator/refresh-indicator.ts";
 import "./components/splash-indicator/splash-indicator.ts";
 import "./components/refresh-button/refresh-button.ts";
 
+import "./elements/navigation-bar/navigation-bar.ts";
+import "./elements/overlay-manager/overlay-manager.ts";
+
+import "./screens/category-screen/category-screen.ts";
+import "./screens/dashboard-screen/dashboard-screen.ts";
+import "./screens/entity-archive-screen/entity-archive-screen.ts";
+import "./screens/entity-detail-screen/entity-detail-screen.ts";
+import "./screens/investment-account-detail-screen/investment-account-detail-screen.ts";
+import "./screens/investment-accounts-screen/investment-accounts-screen.ts";
+import "./screens/investment-overview-screen/investment-overview-screen.ts";
+import "./screens/import-screen/import-screen.ts";
+import "./screens/people-screen/people-screen.ts";
+import "./screens/settings-screen/settings-screen.ts";
+import "./screens/sync-screen/sync-screen.ts";
 import "./screens/transactions/transactions.ts";
+import "./screens/vendors-screen/vendors-screen.ts";
 ////
 
 (function () {})();
