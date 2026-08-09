5c879d1a439835b36995e6fddb5be79047799769
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-08-09 14:56:51 -0400

    Update checkbox.ts
    Added dedicated listener to the checkbox

--- src/components/checkbox/checkbox.ts
+++ src/components/checkbox/checkbox.ts
@@ -1,6 +1,8 @@
 import { getIcon } from "../../icons";
+import { createEventHandler } from "../../utilities/event-utilities";
 
 export class Checkbox extends HTMLElement {
+  #listening = false;
   #checkmark!: HTMLElement;
   #isOn: boolean = false;
 
@@ -10,6 +12,29 @@ export class Checkbox extends HTMLElement {
     this.append(icon);
 
     this.#handleToggle();
+
+    if (!this.#listening) {
+      this.addEventListener("click", this);
+      this.#listening = true;
+    }
+  }
+
+  diconnectedCallback() {
+    if (!this.#listening) {
+      this.removeEventListener("click", this);
+      this.#listening = false;
+    }
+  }
+
+  handleEvent(event: Event) {
+    switch (event.type) {
+      case "click":
+        this.isOn = !this.isOn;
+        break;
+
+      default:
+        break;
+    }
   }
 
   #handleToggle() {
@@ -20,11 +45,18 @@ export class Checkbox extends HTMLElement {
   set isOn(value: boolean) {
     this.#isOn = value;
     this.#handleToggle();
+    this.#events.dispatch({ isOn: this.#isOn }, { bubbles: true });
   }
 
   get isOn(): boolean {
     return this.#isOn;
   }
+
+  #events = createEventHandler("checkbox-selection", this);
+
+  addListener = this.#events.addListener;
+  removeListener = this.#events.removeListener;
+  handleCheckboxSelection = this.#events.handleEvent;
 }
 
 customElements.define("check-box", Checkbox);
