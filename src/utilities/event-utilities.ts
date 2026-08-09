0d4cd4149f3876f707ed960d75378a6d9a40351d
drjacobharden <100780777+drjacobharden@users.noreply.github.com>
2026-08-09 14:56:25 -0400

    Update event-utilities.ts
    I expanded the event utilities with individual typesafe functions that can be passed to parent components.

--- src/utilities/event-utilities.ts
+++ src/utilities/event-utilities.ts
@@ -1,18 +1,47 @@
-interface Listener<T> extends EventListenerObject {
-  handleEvent(event: CustomEvent<T>): void;
+import { AppliedFilter } from "../components/filter-bar/filter-bar";
+import { DatePickerStep, DateRange } from "./date-utilities";
+
+interface Listener extends EventListenerObject {
+  handleEvent(event: CustomEvent): void;
 }
 
-export const createEventHandler = <T extends CustomEvent>(
-  eventName: string,
+type EventNames = keyof EventDetails;
+type EventDetails = Readonly<{
+  "date-range-changed": { range: DateRange; step: DatePickerStep };
+  "checkbox-selection": { isOn: boolean };
+  "dropdown-selection": { value: string; title: string };
+  "filters-changed": { filters: AppliedFilter<any>[] };
+}>;
+
+export const addListener = (
+  eventName: EventNames,
+  target: EventTarget,
+  listener: Listener,
+) => target.addEventListener(eventName, listener);
+
+export const removeListener = (
+  eventName: EventNames,
+  target: EventTarget,
+  listener: Listener,
+) => target.removeEventListener(eventName, listener);
+
+export const handleCustomEvent = <T extends EventNames>(
+  eventName: T,
+  event: Event,
+  fxn: (detail: EventDetails[T]) => void,
+) => {
+  const e = event as CustomEvent;
+  fxn(e.detail);
+};
+
+export const createEventHandler = <T extends EventNames>(
+  eventName: T,
   target: EventTarget,
 ) => ({
-  dispatch: (detail: T["detail"]) =>
-    target.dispatchEvent(new CustomEvent<T>(eventName, { detail })),
-  addListener: (fxn: Listener<T>) => target.addEventListener(eventName, fxn),
-  removeListener: (fxn: Listener<T>) =>
-    target.removeEventListener(eventName, fxn),
-  handleEvent: (event: Event, handler: (e: T) => void) => {
-    const e = event as T;
-    handler(e);
-  },
+  dispatch: (detail: EventDetails[T], bubbles = { bubbles: false }) =>
+    target.dispatchEvent(new CustomEvent(eventName, { detail, ...bubbles })),
+  addListener: (fxn: Listener) => addListener(eventName, target, fxn),
+  removeListener: (fxn: Listener) => removeListener(eventName, target, fxn),
+  handleEvent: (event: Event, handler: (details: EventDetails[T]) => void) =>
+    handleCustomEvent(eventName, event, handler),
 });
